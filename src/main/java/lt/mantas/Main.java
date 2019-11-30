0dac8640e94497e905139d9542e527540fd727bb
Mantas Gineika <mgineika@yahoo.com>
2019-11-30 08:50:48 +0200

    bandymas5

--- src/main/java/lt/mantas/Main.java
+++ src/main/java/lt/mantas/Main.java
@@ -48,7 +48,6 @@ public class Main {
             }
             double vidurkis = z / list.size();
             System.out.println(vidurkis);
-            System.out.println(z);
 
             em.getTransaction().commit();
         } catch (Exception e) {
